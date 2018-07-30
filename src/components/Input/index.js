baf6c926a42a2aab4cfc7bc6e29d30bf63c090a3
luan0ap <luanap.kun@gmail.com>
2018-07-30 14:41:44 -0300

    Created Form element

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,5 +1,5 @@
 import './style.css'
 
-
+const Input = listID => `<input name="auto" class="input" placeholder="Girl name" list=${listID} data-js="input">`
 
 export default Input
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import Form from './components/Form'
+import Input from './components/Input'
 const $App = document.querySelector('[data-js="App"]')
+
+$App.innerHTML = Form(Input('hello'))
